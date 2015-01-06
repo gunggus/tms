3ea192b514b1d5e85f0db48adafd8d259d3a1e6f
kadeksastrawan <dexas90@gmail.com>
2015-01-06 21:48:53 +0800

    logout

--- wp-content/themes/ods/templates/navbar.php
+++ wp-content/themes/ods/templates/navbar.php
@@ -17,6 +17,9 @@
                     <?php echo anchor('task/manage/closed_task',' <div class="icon"><span class="ico-zip"></span></div><div class="name">Closed Task</div>', 'class="button red"');?>
 				</li>
                 <li>
+                    <?php echo anchor('task/manage/performance',' <div class="icon"><span class="ico-time"></span></div><div class="name">Performance</div>', 'class="button red"');?>
+				</li>
+				<li>
                     <?php echo anchor('admin/module',' <div class="icon"><span class="ico-collapse"></span></div><div class="name">Module</div>', 'class="button red"');?>
 				</li>
 				<li>
@@ -25,33 +28,7 @@
                 <li>
                     <?php echo anchor('task/manage/form_search',' <div class="icon"><span class="ico-search"></span></div><div class="name">Search</div>', 'class="button red"');?>
 				</li>
-                <!--
-				<li class="active">
-                    <a href="#" class="button dblue">
-                        <div class="icon">
-                            <span class="ico-gear"></span>
-                        </div>                    
-                        <div class="name">Setting</div>
-                    </a> 
-                    <ul class="sub">
-                        <li><?php echo anchor("ioc/document/type","Type"); ?></li>
-                        <li><?php echo anchor("ioc/document/access","Access"); ?></li>
-                    </ul>                                        
-                </li>
-				<li>
-                    <a href="#" class="button dblue">
-                        <div class="icon">
-                            <span class="ico-user"></span>
-                        </div>                    
-                        <div class="name">User</div>
-                    </a> 
-                    <ul class="sub">
-                        <li><a href="tables.html">Simple</a></li>
-                        <li><a href="tables_dynamic.html">Dynamic</a></li>
-                    </ul>                                        
-                </li>
-                -->
-				<li>
+                <li>
                     <div class="user">
                         <a href="#" class="name">
                             <?php if($this->session->userdata('logged_in')) { 
@@ -59,35 +36,13 @@
 							"<span>".$this->session->userdata['logged_in']['ui_nama']."</span>
                             <span class='sm'>".$this->session->userdata['logged_in']['ui_level']."</span>
 							","class='name'");
+							echo anchor("user/logout", 
+							"<span class='ico-signout' title='logout'>logout</span>
+                            ","class='name'");
 							} ?>
 						</a>
                     </div>
-					<!--
-                    <div class="buttons">
-                        <div class="sbutton green navButton">
-                            <a href="#"><span class="ico-align-justify"></span></a>
-                        </div>
-                        <div class="sbutton blue">
-                            <a href="#"><span class="ico-cogs"></span></a>
-                            <div class="popup">
-                                <div class="arrow"></div>
-                                <div class="row-fluid">
-                                    <div class="row-form">
-                                        <div class="span12"><strong>SETTINGS</strong></div>
-                                    </div>                                    
-                                    <div class="row-form">
-                                        <div class="span4">Navigation:</div>
-                                        <div class="span8"><input type="radio" class="cNav" name="cNavButton" value="default"/> Default <input type="radio" class="cNav" name="cNavButton" value="bordered"/> Bordered</div>
-                                    </div>                                    
-                                    <div class="row-form">
-                                        <div class="span4">Content:</div>
-                                        <div class="span8"><input type="radio" class="cCont" name="cContent" value=""/> Responsive <input type="radio" class="cCont" name="cContent" value="fixed"/> Fixed</div>
-                                    </div>                                    
-                                </div>
-                            </div>
-                        </div>                        
-                    </div>
-					-->
+					
                 </li>                
             </ul>
             
